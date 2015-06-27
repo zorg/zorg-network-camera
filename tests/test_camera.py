5c88f87122c81e4071a78fdf4600eaeb8c2eae50
Gunther Cox <gunthercx@gmail.com>
2015-06-27 06:55:11 -0400

    Image is cached locally.
    This change should help process images on devices with possibly
    limited processing capabilities such as the Raspberry Pi or
    Intel Edison.
    
    The driver now makes a HEAD request to check if the remote image
    has changed before it will actually download the image. The HEAD
    request should go through much quicker then downloading the whole
    image into memory locally and checking it from there.

--- tests/test_camera.py
+++ tests/test_camera.py
@@ -6,14 +6,65 @@ class TestCamera(TestCase):
 
     def setUp(self):
         options = {
-            "url": "http://salvius.org/test.jpg"
+            "url": "http://www.gravatar.com/avatar/0?d=mm"
         }
         self.camera = Camera(options, None)
 
     def test_get_url(self):
-        self.assertEqual(self.camera.get_url(), "http://salvius.org/test.jpg")
+        """
+        Test that the url of the image is returned.
+        """
+        self.assertEqual(
+            self.camera.get_url(),
+            "http://www.gravatar.com/avatar/0?d=mm"
+        )
+
+    def test_get_image(self):
+        """
+        Test that the image file can be downloaded.
+        """
+        import os
+        image_path = self.camera.download_image()
+        self.assertTrue(os.path.exists(image_path))
+
+        # Clean up after testing
+        os.remove(image_path)
+
+    def test_has_changed(self):
+        """
+        Test that False is returned when the
+        image is checked for the first time.
+        """
+        response = self.camera.has_changed()
+        self.assertTrue(response)
+        self.assertFalse(self.camera.image_last_modified is None)
+
+    def test_has_changed_modified(self):
+        """
+        Test that True is returned when the
+        last modified header changes.
+        """
+        pass
+
+    def test_has_changed_not_modified(self):
+        """
+        Test that False is returned when the
+        last modified header has not changed.
+        """
+        pass
+
+    def test_has_changed_no_modified_header(self):
+        """
+        Test that True is returned when the
+        last modified header is not present.
+        """
+        pass
 
     def test_average_brightness(self):
-        #brightness = self.camera.average_brightness()
-        #self.assertEqual(brightness, 0)
-        pass
+        """
+        Test that a value for the average
+        brightness of the image is returned.
+        """
+        brightness = self.camera.average_brightness()
+        self.assertTrue(brightness > 0)
+
--- zorg_network_camera/camera.py
+++ zorg_network_camera/camera.py
@@ -1,7 +1,6 @@
 from zorg.driver import Driver
-from urllib2 import urlopen
 from PIL import Image, ImageStat
-import StringIO
+import urllib2, urlparse
 
 
 class Camera(Driver):
@@ -10,6 +9,8 @@ class Camera(Driver):
         super(Camera, self).__init__(options, connection)
 
         self.url = options.get("url", "")
+        self.image_last_modified = None
+        self.image_cache_path = None
 
         self.commands = [
             "get_url",
@@ -22,18 +23,56 @@ class Camera(Driver):
         """
         return self.url
 
-    def get_image(self):
+    def download_image(self):
         """
-        Downloads and returns an image object.
+        Download the image and return the
+        local path to the image file.
         """
-        image_url = urlopen(self.url)
-        image_string = StringIO.StringIO(image_url.read())
-        return Image.open(image_string)
+        split = urlparse.urlsplit(self.url)
+        filename = split.path.split("/")[-1]
+
+        data = urllib2.urlopen(self.url)
+        with open(filename, "wb") as image:
+            image.write(data.read())
+
+        return filename
+
+    def has_changed(self):
+        """
+        Method to check if an image has changed
+        since it was last downloaded. By making
+        a head request, this check can be done
+        quicker that downloading and processing
+        the whole file.
+        """
+        request = urllib2.Request(self.url)
+        request.get_method = lambda : 'HEAD'
+
+        response = urllib2.urlopen(request)
+        information = response.info()
+
+        if 'Last-Modified' in information:
+            last_modified = information['Last-Modified']
+
+            # Return False if the image has not been modified
+            if last_modified == self.image_last_modified:
+                return False
+
+        self.image_last_modified = last_modified
+
+        # Return True if the image has been modified
+        # or if the image has no last-modified header
+        return True
 
     def average_brightness(self):
         """
         Return the average brightness of the image.
         """
-        converted_image = self.get_image().convert('L')
+        # Only download the image if it has changed
+        if self.has_changed():
+            self.image_cache_path = self.download_image()
+
+        converted_image = Image.open(self.image_cache_path).convert('L')
         statistics = ImageStat.Stat(converted_image)
         return statistics.mean[0]
+
