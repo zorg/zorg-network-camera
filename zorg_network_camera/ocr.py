02c8707a098c238f9c7658fac0cc45cc58681f60
Gunther Cox <gunthercx@gmail.com>
2015-11-24 20:43:45 -0500

    Removed image queueing thread.
    This background process shouldn't be needed because having a queue
    of recently read text items doesn't provide any benefit when
    checking to see what the current visible text is.

--- zorg_network_camera/ocr.py
+++ zorg_network_camera/ocr.py
@@ -1,30 +1,18 @@
 from zorg.driver import Driver
 import textract
 
-from Queue import LifoQueue
-import threading
-import time
-
 
 class OCR(Driver):
 
     def __init__(self, options, connection):
         super(OCR, self).__init__(options, connection)
 
-        # The number of read items stored in the queue
-        self.queue_size = options.get("queue_size", 10)
-
-        # The frequency of how often to read from the camera (ms)
-        self.read_frequency = options.get("read_frequency", 50)
-
-        # A queue of the last text items read by the camera
-        self.text_queue = LifoQueue(maxsize=self.queue_size)
-
-        self.thread = threading.Thread(target=self.run, args=())
-        self.thread.daemon = True
+        # An attribute to hold the last text item read by the camera
+        self.text_cache = ""
 
         self.commands = [
             "read",
+            "text_visible"
         ]
 
     def read(self):
@@ -33,16 +21,13 @@ class OCR(Driver):
         """
 
         # Only download the image if it has changed
-        if not self.connection.has_changed():
-            return self.text_queue[0]
+        if self.connection.has_changed():
+            image_path = self.connection.download_image()
+            self.text_cache = textract.process(image_path, encoding='ascii')
 
-        image_path = self.connection.download_image()
+        return self.text_cache
 
-        text = textract.process(image_path, encoding='ascii')
-
-        return text
-
-    def is_valid_text(self, text):
+    def text_visible(self):
         """
         Returns true or false based on if the OCR process has read
         actual words. This is needed to prevent non-words from being
@@ -51,10 +36,10 @@ class OCR(Driver):
         """
 
         # Split the input string at points with any amount of whitespace
-        words = text.split()
+        words = self.read().split()
 
         # Light weight check to see if a word exists
-        for word in text:
+        for word in words:
 
             # If the word is a numeric value
             if word.lstrip('-').replace('.','',1).isdigit():
@@ -65,33 +50,4 @@ class OCR(Driver):
                 return True
 
         return False
-
-    def run(self):
-        """
-        Method that runs in the background to check if any text is
-        detected in the current image being captured by the camera.
-        """
-        while True:
-            text = self.read()
-
-            # Make sure the text is not already in the queue
-            if text != self.text_queue[0]:
-                if self.is_valid_text(text):
-                    self.text_queue.put(text)
-
-            time.sleep(self.read_frequency)
-
-    def start(self):
-        """
-        Start the driver. This begins the process of periodically
-        checking the camera input for written text.
-        """
-        self.thread.start()
-
-    def halt(self):
-        """
-        Stop the driver. This begins the process of periodically
-        checking the camera input for written text.
-        """
-        self.thread.stop()
 
