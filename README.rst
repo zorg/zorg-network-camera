330b30a1a5f4a468b2602deb65f61259ea98f552
Gunther Cox <gunthercx@gmail.com>
2016-04-05 20:29:42 -0400

    Removed pandoc dependency.

---
+++ README.rst
@@ -0,0 +1,63 @@
+Zorg Network Camera
+===================
+
+|PYPI| |Build Status| |Coverage Status|
+
+This module contains device adaptors and drivers that make it possible
+to connect network cameras to your robot.
+
+Installation
+------------
+
+::
+
+    pip install zorg-network-camera
+
+Network Camera Adaptor
+----------------------
+
+This module's network camera adaptor handles the basic functions of
+retrieving images from the remote camera when necessary. Because this
+module is intended to run on devices such as the Intel Edison or the
+Raspberry Pi, the adaptor has been designed with additional features for
+efficiency. Mainly, if an images are cached until the image from the
+remote camera has been updated. This ensures that an image will not be
+unnecessarily downloaded onto the device.
+
+Camera Feed Driver
+~~~~~~~~~~~~~~~~~~
+
+The ``Feed`` module provides a driver for accessing the url to of the
+remote camera's image. This is a convenience method intended to be used
+to load the image into your application.
+
+Light Sensor Driver
+~~~~~~~~~~~~~~~~~~~
+
+The ``LightSensor`` module allows you to use the camera as a light
+sensor. This driver provides the ability to get the average lighting
+level from the camera.
+
+OCR Driver
+~~~~~~~~~~
+
+Optical character recognition (OCR) is the process of programmatically
+converting images of typed, handwritten or printed text into
+machine-encoded text. The ``OCR`` driver module provided in this package
+provides a utility that makes it possible for your robot to process
+written text that it sees.
+
+The OCR driver achieves the process of extracting text from an image
+through the use of the open source `Tesseract
+OCR <https://github.com/tesseract-ocr/tesseract>`__ module. Tesseract is
+an optical character recognition engine origionally developed by
+Hewlett-Packard. Development of Tesseract has been `sponsored by Google
+since
+2006 <http://googlecode.blogspot.com/2006/08/announcing-tesseract-ocr.html>`__.
+
+.. |PYPI| image:: https://img.shields.io/pypi/v/zorg-network-camera.svg
+   :target: https://pypi.python.org/pypi/zorg-network-camera/
+.. |Build Status| image:: https://travis-ci.org/zorg/zorg-network-camera.svg?branch=0.0.1
+   :target: https://travis-ci.org/zorg/zorg-network-camera
+.. |Coverage Status| image:: https://coveralls.io/repos/github/zorg/zorg-network-camera/badge.svg?branch=master
+   :target: https://coveralls.io/github/zorg/zorg-network-camera?branch=master
--- setup.py
+++ setup.py
@@ -2,48 +2,45 @@
 # -*- coding: utf-8 -*-
 
 from setuptools import setup, find_packages
+import os
 
-try:
-    from pypandoc import convert
-    readme = lambda f: convert(f, "rst")
-except ImportError:
-    print("Module pypandoc not found, could not convert Markdown to RST")
-    readme = lambda f: open(f, "r").read()
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-req = open("requirements.txt")
+req = open('requirements.txt')
 requirements = req.readlines()
 req.close()
 
 version_string = __import__('zorg_network_camera').__version__
 
 setup(
-    name="zorg-network-camera",
+    name='zorg-network-camera',
     version=version_string,
-    url="https://github.com/zorg/zorg-network-camera",
-    description="A module which includes various network camera utilities.",
-    long_description=readme("README.md"),
-    author="Zorg Group",
-    author_email="gunthercx@gmail.com",
+    url='https://github.com/zorg/zorg-network-camera',
+    description='A module which includes various network camera utilities.',
+    long_description=read('README.rst'),
+    author='Zorg Group',
+    author_email='gunthercx@gmail.com',
     packages=find_packages(),
-    package_dir={"zorg_network_camera": "zorg_network_camera"},
+    package_dir={'zorg_network_camera': 'zorg_network_camera'},
     install_requires=requirements,
-    license="MIT",
+    license='MIT',
     zip_safe=True,
-    platforms=["any"],
-    keywords=["zorg", "network", "ip", "camera"],
+    platforms=['any'],
+    keywords=['zorg', 'network', 'ip', 'camera'],
     classifiers=[
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Environment :: Console",
-        "Environment :: Web Environment",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
     ],
-    test_suite="tests",
+    test_suite='tests',
     tests_require=[]
 )
